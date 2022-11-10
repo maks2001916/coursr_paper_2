db6afd7ff2a172dde71626cc104d900683431644
maks <mamonov.maksim.20003@gmail.com>
2022-11-10 18:21:49 +0500

    внедрено поле LocalDateTime.

--- src/Note.java
+++ src/Note.java
@@ -1,37 +1,22 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Notes implements Repeatability {
+public class Note implements Repeatability {
     private String heading;
     private String description;
     private boolean taskType;
-    private String date;
-    private final int year = 2022;
-    private final int month = 11;
-    private final int day = 5;
-    private int id;
+    LocalDateTime taskDateTime;
+
+    private final int id;
     private int tupeRepeatability;
     Random random;
-    public Notes() {
-        date = String.valueOf(LocalDate.now());
+    public Note() {
         random = new Random();
         id = random.nextInt(10000);
-
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public int getDay() {
-        return day;
     }
 
     public int getTupeRepeatability() {
@@ -40,10 +25,6 @@ public class Notes implements Repeatability {
 
     public void setTupeRepeatability(int tupeRepeatability) {
         this.tupeRepeatability = tupeRepeatability;
-    }
-
-    public String getDate() {
-        return date;
     }
 
     public String getHeading() {
@@ -78,7 +59,7 @@ public class Notes implements Repeatability {
         this.description = description;
     }
 
-    public void inputTasks(Scanner scanner,Notes notes, DailyPlanner dailyPlanner,
+    public void inputTasks(Scanner scanner,Note notes, DailyPlanner dailyPlanner,
                                   ArrayList<Integer> keysContains) {
         System.out.print("Введите название задачи: ");
         dailyPlanner.setNotesContain(notes.getId(), notes);
@@ -93,7 +74,7 @@ public class Notes implements Repeatability {
                     repeatStatusOne(date, dailyPlanner, keisKontains);
                     break;
                 case 2:
-                    repeatStatusDaily(date,dailyPlanner,keisKontains);
+                    repeatStatusDaily(dailyPlanner,keisKontains);
                     break;
                 case 3:
                     repeatStatusWeekly(date, dailyPlanner, keisKontains);
@@ -112,9 +93,7 @@ public class Notes implements Repeatability {
 
     public static void repeatStatusOne(String date, DailyPlanner dailyPlanner, ArrayList<Integer> keisKontains) {
         for (int i = 0; i < dailyPlanner.notesContain.size(); i++) {
-            String dates = String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).year) + '-' +
-                    String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).month) + '-' +
-                    String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).day);
+            String dates = Integer.toString(dailyPlanner.notesContain.get(keisKontains.get(i)).taskDateTime.getDayOfMonth());
             if (date.equals(dates)) {
                 System.out.println(dailyPlanner.notesContain.get(keisKontains.get(i)).toString());
             }
@@ -122,7 +101,7 @@ public class Notes implements Repeatability {
 
     }
 
-    public static void repeatStatusDaily(String date, DailyPlanner dailyPlanner, ArrayList<Integer> keisKontains) {
+    public static void repeatStatusDaily(DailyPlanner dailyPlanner, ArrayList<Integer> keisKontains) {
         for (int i = 0; i < dailyPlanner.notesContain.size(); i++) {
             System.out.println(dailyPlanner.notesContain.get(keisKontains.get(i)).toString());
         }
@@ -132,7 +111,7 @@ public class Notes implements Repeatability {
         for (int i = 0; i < dailyPlanner.notesContain.size(); i++) {
             int dateVariable = date.charAt(dailyPlanner.notesContain.size()-1) +
                     date.charAt(dailyPlanner.notesContain.size());
-            if ((dailyPlanner.notesContain.get(keisKontains.get(i)).day - dateVariable) % 7 == 0 ) {
+            if ((dailyPlanner.notesContain.get(keisKontains.get(i)).taskDateTime.getDayOfYear() - dateVariable) % 7 == 0 ) {
                 System.out.println(dailyPlanner.notesContain.get(keisKontains.get(i)).toString());
             }
         }
@@ -143,7 +122,7 @@ public class Notes implements Repeatability {
             int dateVariable = date.charAt(dailyPlanner.notesContain.size()-1) +
                     date.charAt(dailyPlanner.notesContain.size());
             String dateVariables = String.valueOf(dateVariable);
-            String dataVariableTwo = String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).day);
+            String dataVariableTwo = String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).taskDateTime.getDayOfMonth());
             if (dataVariableTwo.equals(dateVariables)) {
                 System.out.println(dailyPlanner.notesContain.get(keisKontains.get(i)).toString());
             }
@@ -157,8 +136,8 @@ public class Notes implements Repeatability {
             int dateVariables = date.charAt(dailyPlanner.notesContain.size() - 1) +
                     date.charAt(dailyPlanner.notesContain.size());
             String dateIntermediate = String.valueOf(dateVariable) + '-' + String.valueOf(dateVariables);
-            String dataVariableTwo = String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).month) +
-                    '-' + String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).day);
+            String dataVariableTwo = String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).taskDateTime.getMonthValue()) +
+                    '-' + String.valueOf(dailyPlanner.notesContain.get(keisKontains.get(i)).taskDateTime.getDayOfMonth());
             if (dateIntermediate.equals(dataVariableTwo) ) {
                 System.out.println(dailyPlanner.notesContain.get(keisKontains.get(i)).toString());
             }
@@ -169,13 +148,13 @@ public class Notes implements Repeatability {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Notes notes = (Notes) o;
-        return taskType == notes.taskType && id == notes.id && tupeRepeatability == notes.tupeRepeatability && Objects.equals(heading, notes.heading) && Objects.equals(description, notes.description) && Objects.equals(date, notes.date) && Objects.equals(random, notes.random);
+        Note notes = (Note) o;
+        return taskType == notes.taskType && id == notes.id && tupeRepeatability == notes.tupeRepeatability && Objects.equals(heading, notes.heading) && Objects.equals(description, notes.description) && Objects.equals(random, notes.random);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(heading, description, taskType, date, id, tupeRepeatability, random);
+        return Objects.hash(heading, description, taskType, id, tupeRepeatability, random);
     }
 
     @Override
@@ -184,9 +163,7 @@ public class Notes implements Repeatability {
                 "heading='" + heading + '\'' +
                 ", description='" + description + '\'' +
                 ", taskType=" + taskType +
-                ", date='" + String.valueOf(year) + '-' +
-                String.valueOf(month) + '-' +
-                String.valueOf(day) + '\'' +
+                ", date='" + taskDateTime.toString() +
                 ", id=" + id +
                 ", tupeRepeatability=" + tupeRepeatability +
                 '}';
